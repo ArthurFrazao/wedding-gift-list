623f64ae5e46e92531c7593ee8df2f10d62f7d4f
Lucas Frazao <lucasfraza0@outlook.com>
2023-01-15 12:42:46 -0300

    feature: install lib axios

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,15 @@
+import axios from 'axios'
+import { useEffect } from 'react'
 import { HeroCard } from './components/HeroCard'
 import { PageDefault } from './components/PageDefault'
 
 function App() {
+  useEffect(() => {
+    axios
+      .get('http://127.0.0.1:5000/gift-all')
+      .then(resp => console.log('resp', resp))
+  }, [])
+
   return (
     <div>
       <PageDefault>
