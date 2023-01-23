07615f7bb7d1ce383760e6fd34b6efc19d20a0c1
ArthurFrazao <arthurxxfrazao@hotmail.com>
2023-01-23 03:46:54 -0300

    feature: created class BigqueryClass, added envs on Dockerfile and added libs on requirements.txt

--- back/routes/main.py
+++ back/routes/main.py
@@ -1,22 +1,19 @@
 from database import BigQueryClass
 from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
-from google.cloud import bigquery
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 cors = CORS(app, resources={"*": {"origins": "http://localhost:port"}})
 
+bigquery = BigQueryClass()
+
 @app.route("/gift-all", methods=["GET"])
 @cross_origin(origin="localhost", headers=["Content- Type","Authorization"])
 def get_gift_all():
-    client = bigquery.Client()
-    query = "SELECT * FROM `wedding-website2023.backend.gifts`"
-    query_job = client.query(query)
-    results = query_job.result()
-    gifts = [dict(row) for row in results]
-    return jsonify(gifts)
+    results = bigquery.execute_query(query="SELECT * FROM `wedding-website2023.backend.gifts`")
+    return jsonify(results)
 
 
 if __name__ == "__main__":
