93331bf8cc311d57aa901d9685b75ea55cff5d7a
ArthurFrazao <arthurxxfrazao@hotmail.com>
2023-01-23 03:13:42 -0300

    feature: added query job on BigQuery

--- back/routes/main.py
+++ back/routes/main.py
@@ -1,5 +1,7 @@
+from database import BigQueryClass
 from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
+from google.cloud import bigquery
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -9,22 +11,12 @@ cors = CORS(app, resources={"*": {"origins": "http://localhost:port"}})
 @app.route("/gift-all", methods=["GET"])
 @cross_origin(origin="localhost", headers=["Content- Type","Authorization"])
 def get_gift_all():
-    response = jsonify({"teste": "teste"})
-    return response
-
-
-@app.route("/confirm-presence", methods=["GET"])
-@cross_origin(origin="localhost", headers=["Content- Type","Authorization"])
-def get_names():
-    response = jsonify({"teste2": "teste2"})
-    return response
-
-
-@app.route("/ceremony", methods=["GET"])
-@cross_origin(origin="localhost", headers=["Content- Type","Authorization"])
-def get_ceremony():
-    response = jsonify({"teste3": "teste3"})
-    return response
+    client = bigquery.Client()
+    query = "SELECT * FROM `wedding-website2023.backend.gifts`"
+    query_job = client.query(query)
+    results = query_job.result()
+    gifts = [dict(row) for row in results]
+    return jsonify(gifts)
 
 
 if __name__ == "__main__":
