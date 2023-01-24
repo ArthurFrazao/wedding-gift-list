1e5af180193c60a16f362e79e5a2bfcaa883a2e2
ArthurFrazao <arthurxxfrazao@hotmail.com>
2023-01-24 01:59:31 -0300

    (feature): added endpoint gifts-not-presented and renamed endpoint gift-all to all-gifts

--- back/routes/main.py
+++ back/routes/main.py
@@ -9,10 +9,16 @@ cors = CORS(app, resources={"*": {"origins": "http://localhost:port"}})
 
 bigquery = BigQueryClass()
 
-@app.route("/gift-all", methods=["GET"])
+@app.route("/all-gifts", methods=["GET"])
 @cross_origin(origin="localhost", headers=["Content- Type","Authorization"])
-def get_gift_all():
+def get_all_gifts():
     results = bigquery.execute_query(query="SELECT * FROM `wedding-website2023.backend.gifts`")
+    return jsonify(results)
+
+@app.route("/gifts-not-presented", methods=["GET"])
+@cross_origin(origin="localhost", headers=["Content- Type","Authorization"])
+def get_gifts_not_presented():
+    results = bigquery.execute_query(query="SELECT * FROM `wedding-website2023.backend.gifts` WHERE is_presented is false")
     return jsonify(results)
 
 
