42c2ede701cd57f05db83771e24646ac930355cb
ArthurFrazao <arthurxxfrazao@hotmail.com>
2023-01-28 03:08:12 -0300

    feature: added app.after_request to manage access controls

--- back/routes/main.py
+++ back/routes/main.py
@@ -1,5 +1,5 @@
 from database import BigQueryClass
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
@@ -10,16 +10,29 @@ cors = CORS(app, resources={"*": {"origins": "http://localhost:port"}})
 bigquery = BigQueryClass()
 
 @app.route("/all-gifts", methods=["GET"])
-@cross_origin(origin="localhost", headers=["Content- Type","Authorization"])
+# @cross_origin(origin="localhost", headers=["Content- Type","Authorization"])
 def get_all_gifts():
     results = bigquery.execute_query(query="SELECT * FROM `wedding-website2023.backend.gifts`")
     return jsonify(results)
 
-@app.route("/gifts-not-presented", methods=["GET"])
-@cross_origin(origin="localhost", headers=["Content- Type","Authorization"])
-def get_gifts_not_presented():
-    results = bigquery.execute_query(query="SELECT * FROM `wedding-website2023.backend.gifts` WHERE is_presented is false")
-    return jsonify(results)
+# @app.route("/gifts-not-presented", methods=["GET"])
+# @cross_origin(origin="localhost", headers=["Content- Type","Authorization"])
+# def get_gifts_not_presented():
+#     results = bigquery.execute_query(query="SELECT * FROM `wedding-website2023.backend.gifts` WHERE is_presented is false")
+#     return jsonify(results)
+
+# @app.route("/give-gift", methods=["POST"])
+# @cross_origin(origin="localhost", headers=["Content- Type","Authorization"])
+# def give_gift():
+#     payload = request.get_json()
+
+@app.after_request
+def after_request(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE"
+    response.headers["Access-Control-Allow-Headers"] = "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"
+    return response
 
 
 if __name__ == "__main__":
