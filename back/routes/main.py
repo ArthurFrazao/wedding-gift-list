450ca4eced26f5f7adfb621f3864aa74ee3eda13
Arthur Frazao <arthurxxfrazao@hotmail.com>
2023-05-08 02:53:26 -0300

    teste route upload_image

--- back/routes/main.py
+++ back/routes/main.py
@@ -37,17 +37,20 @@ def allowed_file(filename):
 @app.route("/upload-item", methods=["POST"])
 def upload_image():
     
-    file = request.files["file"]
-    print(file.filename)
-    if not file.filename:
-        # flash("No selected file")
-        return jsonify({"error": "No selected file"}), 400
-        # return redirect(request.url)
-    if file and allowed_file(file.filename):
-        print(file)
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-        return jsonify({"message": "File uploaded"}), 200
+    for k in request.files:
+        print(k, request.files[k])
+    return jsonify({"message": "File uploaded"}), 200
+    # file = request.files["file"]
+    # print(file.filename)
+    # if not file.filename:
+    #     # flash("No selected file")
+    #     return jsonify({"error": "No selected file"}), 400
+    #     # return redirect(request.url)
+    # if file and allowed_file(file.filename):
+    #     print(file)
+    #     filename = secure_filename(file.filename)
+    #     file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+    #     return jsonify({"message": "File uploaded"}), 200
         # return redirect(url_for('download_file', name=filename))        
      
 
